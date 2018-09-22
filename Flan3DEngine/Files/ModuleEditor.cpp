3c070ddf4f9eb36bc1d9df5168a7612d2538d951
Jony635 <jony635molina@gmail.com>
2018-09-22 11:28:24 +0200

    Revert "MathGeoLib imported"
    This reverts commit d00560e9a313abf3e4e954764fc331440d7d1392.

--- Flan3DEngine/Files/ModuleEditor.cpp
+++ Flan3DEngine/Files/ModuleEditor.cpp
@@ -1,12 +1,9 @@
 #include "Application.h"
 #include "ModuleEditor.h"
 
-#include "MathGeoLib/MathGeoLib.h"
-
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
 #include "imgui/imgui_impl_opengl2.h"
-
 #include "SDL/include/SDL.h"
 
 ModuleEditor::ModuleEditor(Application* app, bool start_enabled) : Module(app, start_enabled) {}
@@ -28,11 +25,6 @@ bool ModuleEditor::Start()
 	ImGui_ImplOpenGL2_Init();
 
 	ImGui::StyleColorsDark();
-
-
-
-
-
 	return true;	
 }
 
@@ -64,9 +56,6 @@ update_status ModuleEditor::PreUpdate(float dt)
 
 update_status ModuleEditor::Update(float dt)
 {
-	int i = 0;
-	if(App->input->GetKey(SDL_SCANCODE_1) == KEY_STATE::KEY_DOWN)
-		//Random here?
 	return UPDATE_CONTINUE;
 }
 
