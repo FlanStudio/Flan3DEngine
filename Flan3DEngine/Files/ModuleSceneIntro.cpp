79ffe8c6395e10f8bbb0dbdf5603f052f1fdc87a
Jony635 <jony635molina@gmail.com>
2018-10-08 13:55:43 +0200

    cube direct mode

--- Flan3DEngine/Files/ModuleSceneIntro.cpp
+++ Flan3DEngine/Files/ModuleSceneIntro.cpp
@@ -19,11 +19,11 @@ bool ModuleSceneIntro::Start()
 	Debug.Log("Loading Intro assets");
 	bool ret = true;
 
-	/*App->camera->Move(float3(200.0f, 1.0f, 200.0f));
-	App->camera->LookAt(float3(0, 0, 0));*/
+	App->camera->Move(float3(0.0f, 0.0f, 5.0f));
+	App->camera->LookAt(float3(0, 0, 0));
 
-	App->camera->Move(float3(6.55f, -322.21f, 219.16f));
-	App->camera->LookAt(float3(3.33f, -45.71f, 144.40f));
+	/*App->camera->Move(float3(6.55f, -322.21f, 219.16f));
+	App->camera->LookAt(float3(3.33f, -45.71f, 144.40f));*/
 
 
 	//sensor = App->physics->AddBody(s, 0.0f);
@@ -74,6 +74,51 @@ update_status ModuleSceneIntro::PostUpdate(float dt)
 {	
 	//cube.Render();
 	//cube2.Render();
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 0);
+	glVertex3f(0, 1, 0);
+	glVertex3f(1, 0, 0);
+	glVertex3f(1, 1, 0);
+	glVertex3f(0, 1, 0); //FRONT
+
+	glVertex3f(1, 0, 0);
+	glVertex3f(1, 0, 1);
+	glVertex3f(1, 1, 0);
+	glVertex3f(1, 0, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, 1, 0); //RIGHT
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(1, 1, 0);
+	glVertex3f(0, 1, 1);
+	glVertex3f(1, 1, 0);
+	glVertex3f(1, 1, 1);
+	glVertex3f(0, 1, 1); //TOP
+
+	glVertex3f(1, 0, 1);
+	glVertex3f(0, 0, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(0, 0, 1);
+	glVertex3f(0, 1, 1);
+	glVertex3f(1, 1, 1); //BACK
+
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 1);
+	glVertex3f(1, 0, 1);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 1);
+	glVertex3f(1, 0, 0); //BOTTOM
+
+	glVertex3f(0, 0, 1);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 1, 1);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 1, 0);
+	glVertex3f(0, 1, 1); //LEFT
+
+
+	glEnd();
 
 
 	return update_status::UPDATE_CONTINUE;
